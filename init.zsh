dd634d27ff9bc7e1223bb68a2666d00685649f09
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 11:04:03 -0400

    fix(prompt): split p6_return_words word and variable into separate args

--- init.zsh
+++ init.zsh
@@ -11,3 +11,19 @@ p6df::modules::whisper::deps() {
     p6m7g8-dotfiles/p6df-openai
   )
 }
+
+######################################################################
+#<
+#
+# Function: words whisper $OPENAI_API_KEY = p6df::modules::whisper::profile::mod()
+#
+#  Returns:
+#	words - whisper $OPENAI_API_KEY
+#
+#  Environment:	 OPENAI_API_KEY
+#>
+######################################################################
+p6df::modules::whisper::profile::mod() {
+
+  p6_return_words 'whisper' '$OPENAI_API_KEY'
+}
